fd943f8c713ed0f4b76306e13025a3804b685863
CalmProton <39672167+CalmProton@users.noreply.github.com>
2023-10-01 00:48:18 +0300

    dev

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Coding challenges
 
-After downloading the repository, please open the root folder in a CLI and run `npm i` to install all packages.
+After downloading the repository, please open the root folder in a CLI and run `npm i` to install all packages before running code.
 
 ### Is Palindrome - _[isPalindrome.ts](src/general/isPalindrome.ts)_
 
@@ -17,3 +17,7 @@ After downloading the repository, please open the root folder in a CLI and run `
 > Example: `ts-node src/general/duplicateChecker.ts 1 2 3 4 4 5 6 7 1`
 
 ## Revel API Usage
+
+### Find Expensive item - [expensiveEatIn.ts](src/api/expensiveEatIn.ts)
+
+- Usage: Open the script file and enter API key and API secret, change client if needed. Open project folder in the CLI and run `ts-node src/api/expensiveEatIn.ts` with no arguments
---
+++ src/api/retrieveMenu.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const API_KEY = "0113cbd5267847c49fab01658e8c81cb";
+const API_SECRET =
+  "cb4e9fcf0a154a5fbfb70eb42c2bc4d1549aed0a50da47b7b42c2379d0c158c7";
+
+const client = "lt-support";
+const url = `https://${client}.revelup.com/`;
+const establishment = "101";
+
+retrieveMenu();
+
+async function retrieveMenu() {
+  try {
+    const menuObject = await axios.get(
+      `${url}weborders/menu/?establishment=${establishment}&mode=6&name=Online`
+    );
+
+    let products: any[] = [];
+
+    const categories = menuObject.data.data.categories;
+
+    categories.forEach((category: { products: any }) => {
+      const tempProducts = category.products;
+
+      tempProducts.forEach((product: object) => {
+        products.push(product);
+      });
+    });
+
+    products.sort((a, b) => a.price - b.price);
+
+    products.forEach((product) => {
+      console.log(`${product.name} - $${product.price}`);
+    });
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
