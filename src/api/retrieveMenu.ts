2a96388ccabd885a9aa466394d82a53ae7befc26
CalmProton <39672167+CalmProton@users.noreply.github.com>
2023-10-01 02:40:41 +0300

    dev

--- src/api/retrieveMenu.ts
+++ src/api/retrieveMenu.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const API_KEY = "0113cbd5267847c49fab01658e8c81cb";
-const API_SECRET =
-  "cb4e9fcf0a154a5fbfb70eb42c2bc4d1549aed0a50da47b7b42c2379d0c158c7";
-
 const client = "lt-support";
 const url = `https://${client}.revelup.com/`;
 const establishment = "101";
+
+retrieveMenu();
 
 export async function retrieveMenu() {
   try {
